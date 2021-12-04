d55ab62b11b58454404b4dc0d9e8aa9a450f7291
jpark6 <qkstjr09@gmail.com>
2021-12-04 17:34:18 +0900

    Feat: Add scss setting

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import 'ant-design-vue/dist/antd.css';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "@/styles/app.scss";
 </style>
